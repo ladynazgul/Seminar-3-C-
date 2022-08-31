bc8de55315b937ac5d51b95d6fdaac6dfa8b1c52
ladynazgool <ladynazgool@gmail.com>
2022-08-31 16:00:20 +0200

    variant added

---
+++ Task-02_var1/Program.cs
@@ -0,0 +1,41 @@
+﻿// 
+
+Console.WriteLine("Задайте число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+int length = N + 1;
+int[] SumArray = new int[N + 1];
+
+for(int i = 1; i < length; i++)
+{
+	SumArray[i] = i;
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetSum(int N, int[] array)
+{
+    int sum = 0;
+
+    for (int i = 0; i < N + 1; i++)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+PrintArray(SumArray);
+int Sum = GetSum(N, SumArray);
+
+Console.WriteLine($"Сумма чисел диапазона равна: {Sum}");
+
+// Надо еще придумать как сделать так, чтобы само число N тоже входило в диапазон - сделано!
