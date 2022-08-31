e5f5bd102291014e965255d198ff78101f25567b
ladynazgool <ladynazgool@gmail.com>
2022-08-31 13:56:43 +0200

    Task-02 added

---
+++ Task-02/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти сумму чисел от 1 до А
+
+Console.WriteLine("Задайте число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+
+int GetSum(int A)
+{
+    int n = 1;
+    int sum = 0;
+
+    for (int i = 0; i < A; i++)
+    {
+        sum = sum + n;
+        n++;
+    }
+    return sum;
+}
+
+int Sum = GetSum(A);
+
+Console.WriteLine($"Сумма чисел диапазона равна: {Sum}");
