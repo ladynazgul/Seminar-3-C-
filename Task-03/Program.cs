aa3efbcc7500a900a2f2c17de1e6df3ffec5bf79
ladynazgool <ladynazgool@gmail.com>
2022-08-31 19:25:52 +0200

    Task-03 added

---
+++ Task-03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Возведите число А в натуральную степень В, используя цикл
+
+Console.WriteLine("Задайте число: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте показатель степени: ");
+int B = int.Parse(Console.ReadLine() ?? "0");
+
+int Exp(int x, int n)
+{
+	int res = x;
+	for (int i = 1; i < n; i++)
+	{
+		res *= x;
+	}
+	return res;
+}
+
+int num = Exp(A, B);
+Console.WriteLine($"Число {A} в степени {B} равно {num}");
