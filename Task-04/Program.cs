404abec1654de0ced98ede5824d4c8e5287cfcf6
ladynazgool <ladynazgool@gmail.com>
2022-09-01 13:07:17 +0400

    Task-04 added

---
+++ Task-04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+
+int b = a.Length;
+
+Console.WriteLine($"Число {a} имеет {b} цифр");
+
