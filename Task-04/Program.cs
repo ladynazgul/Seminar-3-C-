09109305b06188e942c0c4b9e1449ef1e7185fd8
ladynazgool <ladynazgool@gmail.com>
2022-09-01 13:13:39 +0400

    Task-04 changed

--- Task-04/Program.cs
+++ Task-04/Program.cs
@@ -1,9 +1,14 @@
 ﻿// Определить количество цифр в числе
+// Первый способ
 
-Console.WriteLine("Введите число: ");
-string a = Console.ReadLine();
+// Console.WriteLine("Введите число: ");
+// string a = Console.ReadLine() ?? "0";
 
-int b = a.Length;
+// int b = a.Length;
 
-Console.WriteLine($"Число {a} имеет {b} цифр");
+// Console.WriteLine($"Число {a} имеет {b} цифр");
+
+// Второй способ - то что сделали в группе на семинаре
+
+
 
