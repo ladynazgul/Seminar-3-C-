a3813623cd41c94e2cd3e70ac17d250a31bbbba5
ladynazgool <ladynazgool@gmail.com>
2022-09-04 01:53:18 +0200

    Task-04 changed

--- Task-04/Program.cs
+++ Task-04/Program.cs
@@ -1,4 +1,4 @@
-// Определить количество цифр в числе
+﻿// Определить количество цифр в числе
 // Первый способ
 
 // Console.WriteLine("Введите число: ");
@@ -9,3 +9,34 @@
 // Console.WriteLine($"Число {a} имеет {b} цифр");
 
 // Второй способ - то что сделали в группе на семинаре
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+
+int count = 0;
+int current = A;
+
+while (current >= 1)
+{
+    current = current / 10;
+    count++;
+}
+
+Console.WriteLine(count);
+
+// Третий способ - через символьный тип char - этот код показывает сумму цифр числа, а не их количество!
+
+// Console.Write("Введите число: ");
+// string S = Console.ReadLine() ?? "0";
+
+// int Length = S.Length;
+// int sum = 0;
+
+// for(int i = 0; i < Length; i++)
+// {
+//     char B = S[i];
+//     int A = (int)(B - '0');
+//     sum = sum + A;
+// }
+
+// Console.WriteLine(sum);
