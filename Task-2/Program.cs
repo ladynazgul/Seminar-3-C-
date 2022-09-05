9511e97bd6f720aa14ea8a98aef9b8137904fd26
ladynazgool <ladynazgool@gmail.com>
2022-09-05 02:12:07 +0200

    Task-2 added

---
+++ Task-2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+string S = Console.ReadLine() ?? "0";
+
+int Length = S.Length;
+int sum = 0;
+
+for(int i = 0; i < Length; i++)
+{
+    char B = S[i];
+    int A = (int)(B - '0');
+    sum = sum + A;
+}
+
+Console.WriteLine(sum);
