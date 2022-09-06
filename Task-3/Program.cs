0ca579148bee5604785e9962ecc9c1c34685e06b
ladynazgool <ladynazgool@gmail.com>
2022-09-06 12:52:20 +0400

    Task-3/Program.cs changed

--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -1,1 +1,21 @@
 ﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Задайте число A: ");
+int A = int.Parse(Console.ReadLine() ?? "0");
+
+int GetProd(int A)
+{
+    int n = 1;
+    int prod = 1;
+
+    for (int i = 0; i < A; i++)
+    {
+        prod = prod * n;
+        n++;
+    }
+    return prod;
+}
+
+int Prod = GetProd(A);
+
+Console.WriteLine($"Произведение чисел диапазона равно: {Prod}");
